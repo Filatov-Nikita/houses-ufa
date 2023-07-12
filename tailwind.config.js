31dc73b209e8b5ee702ef0080ec03dec48551071
Nikita Antonov <nikita45454@gmail.com>
2023-07-12 12:57:23 +0300

    feat: добавляет настройки адаптивки

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   prefix: 'tw-',
   content: [
+    "./store/**/*.ts",
     "./components/**/*.{js,vue,ts}",
     "./layouts/**/*.vue",
     "./pages/**/*.vue",
@@ -18,9 +19,17 @@ module.exports = {
       'white': '#ffffff',
       'dark': '#2D2D2D',
       'gray': '#F7F8F8',
+      'gray-600': '#9F9F9F',
       'secondary': '#F0F0F0',
       'secondary-hover': '#E5E5E5',
       'secondary-press': '#EFEFEF',
+      'negative': '#F13F36',
+    },
+    screens: {
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1440px',
+      '2xl': '1920px',
     }
   },
   plugins: [],
