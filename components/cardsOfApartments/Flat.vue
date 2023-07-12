302d22c60cd2b143c7a87337aa976a267a98c493
Ralfik <developeraul@gmail.com>
2023-07-12 16:18:37 +0500

    fix: Обноавил комопнент в соответствии с конфигуратором tailwind

--- components/cardsOfApartments/Flat.vue
+++ components/cardsOfApartments/Flat.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="tw-px-4 tw-py-6 l:tw-p-6 d:tw-p-10 tw-rounded-2xl tw-grid tw-gap-6 tw-bg-white"
+    class="tw-px-4 tw-py-6 lg:tw-p-6 xl:tw-p-10 tw-rounded-2xl tw-grid tw-gap-6 tw-bg-white"
   >
     <div class="tw-flex tw-items-center tw-justify-between">
       <a class="back">
@@ -8,7 +8,7 @@
         <span> Вернуться назад </span>
       </a>
       <div>
-        <div class="tw-hidden l:tw-block">
+        <div class="tw-hidden lg:tw-block">
           <div class="tw-flex tw-gap-3">
             <button class="btn">
               <img
@@ -33,7 +33,7 @@
             </button>
           </div>
         </div>
-        <div class="l:tw-hidden">
+        <div class="lg:tw-hidden">
           <button class="btn">
             <img
               src="/assets/images/icons/dots_three_vertical.svg"
@@ -47,7 +47,7 @@
     <div class="content">
       <div>
         <div
-          class="tw-rounded-2xl tw-px-3 tw-py-8 tw-bg-base00 tw-grid tw-place-content-center l:tw-h-[540px] d:tw-h-[737px]"
+          class="tw-rounded-2xl tw-px-3 tw-py-8 tw-bg-base00 tw-grid tw-place-content-center lg:tw-h-[540px] xl:tw-h-[737px]"
         >
           <img
             format="avif,webp"
@@ -57,20 +57,20 @@
         </div>
       </div>
 
-      <div class="l:tw-px-6 d:tw-px-10 l:tw-py-5.5">
-        <h2 class="tw-text-h5 l:tw-text-h4 tw-mb-2">1-комнатная, 96 м²</h2>
+      <div class="lg:tw-px-6 xl:tw-px-10 lg:tw-py-5.5">
+        <h2 class="tw-text-h5 lg:tw-text-h4 tw-mb-2">1-комнатная, 96 м²</h2>
         <p class="tw-text-text01 tw-mb-6">
           ЖК «Михайловка Green Place», с. Михайловка
         </p>
         <div
-          class="tw-flex tw-justify-between tw-items-center tw-mb-6 l:tw-mb-8"
+          class="tw-flex tw-justify-between tw-items-center tw-mb-6 lg:tw-mb-8"
         >
           <div class="tw-p-2 tw-rounded-lg tw-bg-base00">
             Предчистовая отделка
           </div>
           <p class="tw-text-text02">235 695 ₽ / м2</p>
         </div>
-        <div class="tw-grid tw-gap-4 tw-mb-6 l:tw-mb-8">
+        <div class="tw-grid tw-gap-4 tw-mb-6 lg:tw-mb-8">
           <div
             v-for="(item, index) in floorConf"
             :key="index"
@@ -80,12 +80,12 @@
             <span class="tw-place-self-end tw-text-text02"> 3 </span>
           </div>
         </div>
-        <h2 class="tw-text-h5 l:tw-text-h4 tw-mb-2">12 165 265 ₽</h2>
-        <p class="tw-mb-6 l:tw-mb-8">
+        <h2 class="tw-text-h5 lg:tw-text-h4 tw-mb-2">12 165 265 ₽</h2>
+        <p class="tw-mb-6 lg:tw-mb-8">
           <span class="tw-text-text02">В ипотеку от </span>
           <span class="tw-text-primary">15 659 ₽ / мес</span>
         </p>
-        <div class="tw-grid tw-gap-2 l:tw-gap-4">
+        <div class="tw-grid tw-gap-2 lg:tw-gap-4">
           <button class="tw-h-14 tw-w-full tw-bg-primary tw-text-white">
             Забронировать
           </button>
@@ -119,13 +119,13 @@ const floorConf = [
 .content {
   @apply tw-grid tw-gap-6;
 }
-@screen l {
+@screen lg {
   .content {
     @apply tw-gap-5;
     grid-template-columns: minmax(537px, 62%) minmax(323px, 39%);
   }
 }
-@screen l {
+@screen lg {
   .content {
     @apply tw-gap-5;
     grid-template-columns: minmax(745px, 59%) minmax(435px, 41%);
