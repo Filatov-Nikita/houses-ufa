ceebdf11bd7b3d120bebe5463815fb41ea5b6758
Ralfik <developeraul@gmail.com>
2023-07-13 12:43:16 +0500

    feat: Базовый компонент для иконки

---
+++ components/base/SquareIcon.vue
@@ -0,0 +1,16 @@
+<template>
+  <div
+    class="tw-bg-secondary tw-w-12 tw-h-12 tw-rounded-lg tw-grid tw-place-content-center tw-p-2"
+  >
+    <img :src="src" :alt="alt" :width="width" :height="height" />
+  </div>
+</template>
+<script setup lang="ts">
+const iconProps = defineProps<{
+  src: string
+  alt?: string
+  width?: string
+  height?: string
+}>()
+</script>
+<style lang="scss" scoped></style>
