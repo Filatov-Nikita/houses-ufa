74189905611997e658c5360f098c95c06e320508
Ralfik <developeraul@gmail.com>
2023-07-13 16:15:54 +0500

    feat(components): Обновил позиционирование изображения

--- components/cards/CountryRealEstate.vue
+++ components/cards/CountryRealEstate.vue
@@ -1,44 +1,57 @@
 <template>
-  <div class="">
-    <div class="tw-relative tw-rounded-2xl tw-overflow-hidden tw-mb-4">
+  <div class="card">
+    <div
+      class="tw-relative tw-rounded-2xl tw-overflow-hidden tw-mb-4 tw-bg-base01 tw-grid tw-place-content-center"
+    >
       <img
+        v-if="src"
         :src="src"
         :alt="alt"
         :width="width"
-        :height="height"
-        class="tw-w-full tw-object-cover"
+        :height="374"
+        class="tw-absolute tw-w-full tw-h-full tw-object-cover"
+      />
+      <img
+        v-else
+        src="/assets/images/icons/house.svg"
+        :alt="alt"
+        :width="64"
+        :height="64"
       />
       <div
         v-if="status"
-        class="tw-absolute tw-right-4 tw-top-4 tw-backdrop-blur-sm tw-rounded-lg tw-py-[5px] tw-px-[11px] tw-bg-white/40 tw-text-white tw-text-body_s -tw-tracking-875"
+        class="tw-absolute tw-right-4 tw-top-4 tw-backdrop-blur-sm tw-rounded-lg tw-py-[5px] tw-px-[11px] tw-text-white tw-text-body_s -tw-tracking-875"
+        :class="[src ? 'tw-bg-white/40' : 'tw-bg-text01/40']"
       >
         {{ status }}
       </div>
     </div>
-    <div class="tw-flex tw-justify-between tw-gap-4 tw-mb-2">
-      <h4 class="tw-text-h6 lg:tw-text-h5">{{ title }}</h4>
-      <span class="lg:tw-hidden">
-        <span v-if="price" class="tw-text-text00 tw-whitespace-nowrap">{{
-          price
-        }}</span>
-      </span>
+    <div class="">
+      <div class="tw-flex tw-justify-between tw-gap-4 tw-mb-2">
+        <h4 class="tw-text-h6 lg:tw-text-h5">{{ title }}</h4>
+        <span class="lg:tw-hidden">
+          <span v-if="price" class="tw-text-text00 tw-whitespace-nowrap">{{
+            price
+          }}</span>
+        </span>
+      </div>
+      <p class="tw-text-text02">
+        <span> {{ place }} </span>
+        <span>
+          <span> · </span>
+          <span> {{ destance }} </span>
+        </span>
+        <span v-if="price" class="tw-hidden lg:tw-inline">
+          <span> · </span>
+          <span class="tw-text-text00">{{ price }}</span>
+        </span>
+      </p>
     </div>
-    <p class="tw-text-text02">
-      <span> {{ place }} </span>
-      <span>
-        <span> · </span>
-        <span> {{ destance }} </span>
-      </span>
-      <span v-if="price" class="tw-hidden lg:tw-inline">
-        <span> · </span>
-        <span class="tw-text-text00">{{ price }}</span>
-      </span>
-    </p>
   </div>
 </template>
 <script setup lang="ts">
 const countryProps = defineProps<{
-  src: string
+  src?: string
   alt?: string
   width?: number
   height?: number
@@ -49,4 +62,9 @@ const countryProps = defineProps<{
   price?: string
 }>()
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.card {
+  display: grid;
+  grid-template-rows: 374px auto;
+}
+</style>
