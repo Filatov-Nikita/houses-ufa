f89e9c5ae4f3603d02a2eb90e223d23823da669e
Ralfik <developeraul@gmail.com>
2023-07-13 13:24:43 +0500

    fix: обновил состояние

--- components/base/SquareIcon.vue
+++ components/base/SquareIcon.vue
@@ -2,15 +2,8 @@
   <div
     class="tw-bg-secondary tw-w-12 tw-h-12 tw-rounded-lg tw-grid tw-place-content-center tw-p-2"
   >
-    <img :src="src" :alt="alt" :width="width" :height="height" />
+    <slot />
   </div>
 </template>
-<script setup lang="ts">
-const iconProps = defineProps<{
-  src: string
-  alt?: string
-  width?: string
-  height?: string
-}>()
-</script>
+<script setup lang="ts"></script>
 <style lang="scss" scoped></style>
