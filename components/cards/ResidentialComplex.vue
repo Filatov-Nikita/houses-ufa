fff8f56dce1f5fd8586861eb77f907b16d082cd1
Ralfik <developeraul@gmail.com>
2023-07-15 11:15:03 +0500

    feat: Добавил карточку жилого комплекса

---
+++ components/cards/ResidentialComplex.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="card">
+    <div
+      class="tw-relative tw-rounded-2xl tw-overflow-hidden tw-mb-4 tw-bg-base01 tw-grid tw-place-content-center"
+    >
+      <img
+        v-if="src"
+        :src="src"
+        :alt="alt"
+        :width="width"
+        :height="374"
+        class="tw-absolute tw-w-full tw-h-full tw-object-cover"
+      />
+      <img
+        v-else
+        src="/assets/images/icons/house.svg"
+        :alt="alt"
+        :width="64"
+        :height="64"
+      />
+      <div class="tw-absolute tw-left-4 tw-top-4 tw-grid tw-gap-1">
+        <h4 class="tw-text-h4 tw-text-white">{{ title }}</h4>
+        <p class="tw-text-body_l tw-text-white">
+          <span>{{ typeApartment }}</span>
+          <span> от {{ price }} ₽</span>
+        </p>
+      </div>
+      <div class="tw-absolute tw-right-4 tw-bottom-4 tw-grid tw-gap-3">
+        <div
+          v-if="status"
+          class="tw-backdrop-blur-sm tw-rounded-lg tw-py-[5px] tw-px-[11px] tw-text-white tw-bg-text01/40 tw-text-body_s -tw-tracking-875"
+        >
+          {{ status }}
+        </div>
+        <div
+          v-if="place"
+          class="tw-backdrop-blur-sm tw-rounded-lg tw-py-[5px] tw-px-[11px] tw-text-white tw-bg-text01/40 tw-text-body_s -tw-tracking-875 tw-flex tw-items-center tw-gap-3"
+        >
+          <img
+            src="/assets/images/icons/geo.svg"
+            alt="location"
+            width="24"
+            height="24"
+          />
+          {{ place }}
+        </div>
+      </div>
+      <button class="tw-absolute tw-left-4 tw-bottom-4 button-link">
+        <BaseSquareIcon>
+          <img
+            src="/assets/images/icons/arrow_line_right-green.svg"
+            width="24"
+            height="24"
+          />
+        </BaseSquareIcon>
+      </button>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+const complexProps = defineProps<{
+  src: string
+  alt?: string
+  width?: number
+  height?: number
+  title: string
+  typeApartment: string
+  price: string
+  place: string
+  status: string
+}>()
+</script>
+<style lang="scss" scoped>
+.card {
+  display: grid;
+  grid-template-rows: 374px auto;
+  .button-link {
+    @apply tw-opacity-0;
+    @apply tw-transition-opacity transition-base;
+  }
+  @media (hover: hover) {
+    &:hover .button-link {
+      @apply tw-opacity-100;
+    }
+  }
+
+  @media (hover: none) {
+    &:active .button-link {
+      @apply tw-opacity-100;
+    }
+  }
+}
+</style>
