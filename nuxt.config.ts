643938d075e59267b27d451ba9e01a448dc1df53
Ralfik <developeraul@gmail.com>
2023-07-24 16:28:41 +0500

    feat: создание карт с категориями и комплексами

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: [
-    '@pinia/nuxt',
-  ],
+  modules: ['@pinia/nuxt'],
   css: ['~/assets/css/app.css'],
   postcss: {
     plugins: {
