e79661517b69348e30c063ef4b3841fb3ea28da9
Nikita Antonov <nikita45454@gmail.com>
2023-08-03 15:14:19 +0300

    feat: добавляет стили базового тагла

--- components/base/BaseToggle.vue
+++ components/base/BaseToggle.vue
@@ -1,12 +1,19 @@
 <template>
-  <div class="tw-relative" :class="{ 'tw-pb-6': !hideBottomSpace }">
-    <div class="base-toggle">
-      <div class="toggle" :class="{ 'toggle-active': checked }" @click="handleChange(checkedValue)">
-        <div class="dot" :class="{ 'dot-active': checked }"></div>
+  <div class="base-toggle">
+    <div class="base-toggle__toggle" @click="handleChange(checkedValue)">
+      <div class="base-toggle__input" :class="{ 'base-toggle__input--active': checked }">
+        <div class="base-toggle__dot" :class="{ 'base-toggle__dot--active': checked }"></div>
       </div>
-      <label class="label">{{ label }}</label>
+      <label v-if="label" class="base-toggle__label">{{ label }}</label>
     </div>
-    <div v-if="errorMessage" :class="errorClasses">{{ errorMessage }}</div>
+    <transition
+      leave-active-class="animate__animated anim-300ms animate__fadeOutUp"
+      enter-active-class="animate__animated anim-300ms animate__shakeX"
+    >
+      <div v-if="errorMessage" class="base-toggle__error-msg">
+        {{ errorMessage }}
+      </div>
+    </transition>
   </div>
 </template>
 
@@ -16,17 +23,15 @@
   interface Props {
     name: string,
     rules?: string,
-    label: string,
+    label?: string,
     modelValue?: any,
     checkedValue?: any,
     uncheckedValue?: any,
-    hideBottomSpace?: boolean
   }
 
   const props = withDefaults(defineProps<Props>(), {
     checkedValue: true,
     uncheckedValue: false,
-    hideBottomSpace: false
   });
 
   const { checked, handleChange, errorMessage } = useField(props.name, props.rules, {
@@ -34,65 +39,63 @@
     syncVModel: true,
     checkedValue: props.checkedValue,
     uncheckedValue: props.uncheckedValue,
-  });
-
-  const errorClasses = computed(() => {
-    return [
-      'tw-text-xs',
-      'tw-text-error',
-      'tw-leading-none',
-      {
-        'tw-absolute tw-bottom-1': !props.hideBottomSpace,
-        'tw-mt-2': props.hideBottomSpace,
-      }
-    ];
+    label: props.label
   });
 </script>
-<style scoped>
+<style scoped lang="scss">
   .base-toggle {
-    display: flex;
-    align-items: center;
-    gap: 16px;
-  }
-
-  .toggle {
-    min-width: 48px;
-    height: 24px;
-    border-radius: 12px;
-    border-width: 1px;
-    border-style: solid;
-    border-color: theme('colors.base01');
-    background-color: theme('colors.base01');
-    line-height: 22px;
-    cursor: pointer;
-    padding: 0 4px;
-  }
-
-  .toggle-active {
-    background-color: theme('colors.primary');
-    border-color: theme('colors.primary');
-  }
-
-  .dot {
     display: inline-block;
-    vertical-align: middle;
-    width: 14px;
-    height: 14px;
-    border-radius: 50%;
-    border-width: 2px;
-    border-style: solid;
-    border-color: theme('colors.base01');
-    background-color: theme('colors.base02');
-    transition: transform 300ms;
-  }
 
-  .dot-active {
-    background-color: theme('colors.white');
-    border-color: theme('colors.white');
-    transform: translateX(24px);
-  }
+    &__toggle {
+      display: flex;
+      align-items: center;
+      gap: 16px;
+      user-select: none;
+    }
 
-  .label {
-    @apply tw-text-text00 tw-leading-none tw-text-base;
+    &__input {
+      min-width: 48px;
+      height: 24px;
+      border-radius: 12px;
+      border-width: 1px;
+      border-style: solid;
+      border-color: theme('colors.base01');
+      background-color: theme('colors.base01');
+      line-height: 22px;
+      cursor: pointer;
+      padding: 0 4px;
+
+      &--active {
+        background-color: theme('colors.primary');
+        border-color: theme('colors.primary');
+      }
+    }
+
+    &__dot {
+      display: inline-block;
+      vertical-align: middle;
+      width: 14px;
+      height: 14px;
+      border-radius: 50%;
+      border-width: 2px;
+      border-style: solid;
+      border-color: theme('colors.base01');
+      background-color: theme('colors.base02');
+      transition: transform 300ms;
+
+      &--active {
+        background-color: theme('colors.white');
+        border-color: theme('colors.white');
+        transform: translateX(24px);
+      }
+    }
+
+    &__label {
+      @apply tw-text-text00 tw-leading-none tw-text-base;
+    }
+
+    &__error-msg {
+      @apply tw-mt-2 tw-text-error tw-text-xs tw-font-normal;
+    }
   }
 </style>
