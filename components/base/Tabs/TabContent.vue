a06956254c7d558290f2e60ec5e9b949481e0861
Nikita Antonov <nikita45454@gmail.com>
2023-08-10 15:46:11 +0300

    feat: добавляет компоненты для контента табов

---
+++ components/base/Tabs/TabContent.vue
@@ -0,0 +1,47 @@
+<script lang="ts">
+import { Transition, KeepAlive } from 'vue';
+
+export default defineComponent({
+  name: 'TabContent',
+  props: {
+    modelValue: {
+      required: true,
+    },
+    leaveClasses: {
+      default: 'animate__animated anim-100ms animate__zoomOut',
+      type: String
+    },
+    enterClasses: {
+      default: 'animate__animated anim-100ms animate__zoomIn',
+      type: String
+    },
+    keepAlive: {
+      default: false,
+      type: Boolean
+    }
+  },
+  render() {
+    const slotDefault = this.$slots.default;
+
+    const newSlots = () => {
+      if(slotDefault === undefined) return undefined;
+      const elements = slotDefault();
+      console.log(elements);
+
+      const filterdElements = elements
+        .filter((el) => el.props?.name === this.modelValue);
+
+      return filterdElements.length === 0 ? undefined : filterdElements;
+    };
+
+
+    return h('div',
+      h(Transition, {
+        'mode': 'out-in',
+        'leave-active-class': this.leaveClasses,
+        'enter-active-class': this.enterClasses,
+      }, this.keepAlive ? () => h(KeepAlive, newSlots()) : newSlots),
+    );
+  }
+});
+</script>
---
+++ components/base/Tabs/TabContentItem.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+  const instance = getCurrentInstance();
+  defineProps<{ name: string }>();
+</script>
