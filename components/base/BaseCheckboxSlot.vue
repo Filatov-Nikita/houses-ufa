0868581a9c0fd89c8aa24da0fc0aaa4c60a17f36
Ralfik <developeraul@gmail.com>
2023-08-25 09:14:29 +0500

    feat: Добавил модалку, карточку для модалки и чекбокс со слотом

---
+++ components/base/BaseCheckboxSlot.vue
@@ -0,0 +1,23 @@
+<template>
+  <button type="button" @click="handleChange(value)">
+    <slot v-bind="{ checked }" />
+  </button>
+</template>
+<script lang="ts" setup>
+import { useField } from 'vee-validate'
+
+interface Props {
+  value: number | string
+  modelValue: any
+  name: string
+}
+
+const props = defineProps<Props>()
+const { name } = toRefs(props)
+const { checked, handleChange } = useField(name, undefined, {
+  type: 'checkbox',
+  syncVModel: true,
+  checkedValue: props.value,
+})
+</script>
+<style lang="scss" scoped></style>
