ede694f1528df1d557351b1b7d246df5379f554f
Ralfik <developeraul@gmail.com>
2023-08-30 13:50:56 +0500

    fix: Удалил обёртку

--- components/tab/ContructionProgress.vue
+++ components/tab/ContructionProgress.vue
@@ -6,132 +6,128 @@
       </BaseTabsGroupItem>
     </BaseTabsGroup>
     <BaseTabsTabContent v-model="tab">
-      <BaseTabsTabContentItem name="photos">
-        <div class="tw-relative">
-          <Swiper
-            :modules="[SwiperPagination, SwiperNavigation]"
-            :slides-per-view="1.3"
-            :space-between="20"
-            :centeredSlides="true"
-            @swiper="onSwiper"
-            :breakpoints="{
-              '1024': {
-                slidesPerView: 3,
-                centeredSlides: false,
-              },
-              '1440': {
-                slidesPerView: 4,
-                centeredSlides: false,
-              },
-            }"
-            :pagination="{
-              type: 'bullets',
-              el: '.swiper-pagination',
-              bulletActiveClass: 'dot-active',
-              bulletClass: 'dot',
-            }"
-            :navigation="{
-              nextEl: '.swiper-next',
-              prevEl: '.swiper-prev',
-            }"
-          >
-            <SwiperSlide v-for="n in 10">
-              <div class="card">
-                <img
-                  src="/assets/images/img/country_real_estate.png"
-                  alt=""
-                  class=""
-                />
-                <div class="card__wrapper">
-                  <div class="card__count">12 фото</div>
-                  <div class="tw-grid tw-gap-1 tw-text-white">
-                    <div class="tw-text-h5">Май</div>
-                    <div>2023 г.</div>
-                  </div>
+      <BaseTabsTabContentItem name="photos" key="photos" class="tw-relative">
+        <Swiper
+          :modules="[SwiperPagination, SwiperNavigation]"
+          :slides-per-view="1.3"
+          :space-between="20"
+          :centeredSlides="true"
+          @swiper="onSwiper"
+          :breakpoints="{
+            '1024': {
+              slidesPerView: 3,
+              centeredSlides: false,
+            },
+            '1440': {
+              slidesPerView: 4,
+              centeredSlides: false,
+            },
+          }"
+          :pagination="{
+            type: 'bullets',
+            el: '.swiper-pagination',
+            bulletActiveClass: 'dot-active',
+            bulletClass: 'dot',
+          }"
+          :navigation="{
+            nextEl: '.swiper-next',
+            prevEl: '.swiper-prev',
+          }"
+        >
+          <SwiperSlide v-for="n in 10">
+            <div class="card">
+              <img
+                src="/assets/images/img/country_real_estate.png"
+                alt=""
+                class=""
+              />
+              <div class="card__wrapper">
+                <div class="card__count">12 фото</div>
+                <div class="tw-grid tw-gap-1 tw-text-white">
+                  <div class="tw-text-h5">Май</div>
+                  <div>2023 г.</div>
                 </div>
               </div>
-            </SwiperSlide>
-          </Swiper>
+            </div>
+          </SwiperSlide>
+        </Swiper>
 
-          <button
-            @click="swiper?.slidePrev()"
-            class="tw-absolute -tw-left-4 tw-top-1/2 tw-z-10 tw-transform -tw-translate-y-1/2 tw-rounded-lg tw-h-12 tw-w-12 tw-bg-text01/40 tw-backdrop-blur-sm tw-grid tw-place-content-center"
-          >
-            <BaseIcon name="back" class="tw-w-6 tw-h-6 tw-text-white" />
-          </button>
-          <button
-            @click="swiper?.slideNext()"
-            class="tw-absolute -tw-right-4 tw-top-1/2 tw-z-10 tw-transform -tw-translate-y-1/2 tw-rounded-lg tw-h-12 tw-w-12 tw-bg-text01/40 tw-backdrop-blur-sm tw-grid tw-place-content-center"
-          >
-            <BaseIcon name="forward" class="tw-w-6 tw-h-6 tw-text-white" />
-          </button>
-        </div>
+        <button
+          @click="swiper?.slidePrev()"
+          class="tw-absolute -tw-left-4 tw-top-1/2 tw-z-10 tw-transform -tw-translate-y-1/2 tw-rounded-lg tw-h-12 tw-w-12 tw-bg-text01/40 tw-backdrop-blur-sm tw-grid tw-place-content-center"
+        >
+          <BaseIcon name="back" class="tw-w-6 tw-h-6 tw-text-white" />
+        </button>
+        <button
+          @click="swiper?.slideNext()"
+          class="tw-absolute -tw-right-4 tw-top-1/2 tw-z-10 tw-transform -tw-translate-y-1/2 tw-rounded-lg tw-h-12 tw-w-12 tw-bg-text01/40 tw-backdrop-blur-sm tw-grid tw-place-content-center"
+        >
+          <BaseIcon name="forward" class="tw-w-6 tw-h-6 tw-text-white" />
+        </button>
       </BaseTabsTabContentItem>
-      <BaseTabsTabContentItem name="cameras">
-        <div class="tw-relative">
-          <Swiper
-            :modules="[SwiperPagination, SwiperNavigation]"
-            :slides-per-view="1.3"
-            :space-between="20"
-            :centeredSlides="true"
-            @swiper="onSwiperCamera"
-            :breakpoints="{
-              '1024': {
-                slidesPerView: 3,
-                centeredSlides: false,
-              },
-              '1440': {
-                slidesPerView: 4,
-                centeredSlides: false,
-              },
-            }"
-            :pagination="{
-              type: 'bullets',
-              el: '.swiper-pagination',
-              bulletActiveClass: 'dot-active',
-              bulletClass: 'dot',
-            }"
-            :navigation="{
-              nextEl: '.swiper-next',
-              prevEl: '.swiper-prev',
-            }"
-          >
-            <SwiperSlide v-for="n in 10">
-              <div class="card card-camera">
-                <img
-                  src="/assets/images/img/country_real_estate.png"
-                  alt=""
-                  class=""
-                />
-                <BaseButton
-                  padding-classes="tw-p-4"
-                  class="tw-absolute tw-top-1/2 tw-left-1/2 tw-transform -tw-translate-y-1/2 -tw-translate-x-1/2 tw-w-fit"
-                >
-                  <BaseIcon name="play" class="tw-w-6 tw-h-6" />
-                </BaseButton>
-                <div class="card__wrapper">
-                  <div></div>
-                  <div class="tw-grid tw-gap-1 tw-text-white">
-                    <div class="tw-text-h5">Камера 1</div>
-                  </div>
+      <BaseTabsTabContentItem name="cameras" key="cameras" class="tw-relative">
+        <Swiper
+          :modules="[SwiperPagination, SwiperNavigation]"
+          :slides-per-view="1.3"
+          :space-between="20"
+          :centeredSlides="true"
+          @swiper="onSwiperCamera"
+          :breakpoints="{
+            '1024': {
+              slidesPerView: 3,
+              centeredSlides: false,
+            },
+            '1440': {
+              slidesPerView: 4,
+              centeredSlides: false,
+            },
+          }"
+          :pagination="{
+            type: 'bullets',
+            el: '.swiper-pagination',
+            bulletActiveClass: 'dot-active',
+            bulletClass: 'dot',
+          }"
+          :navigation="{
+            nextEl: '.swiper-next',
+            prevEl: '.swiper-prev',
+          }"
+        >
+          <SwiperSlide v-for="n in 10">
+            <div class="card card-camera">
+              <img
+                src="/assets/images/img/country_real_estate.png"
+                alt=""
+                class=""
+              />
+              <BaseButton
+                padding-classes="tw-p-4"
+                class="tw-absolute tw-top-1/2 tw-left-1/2 tw-transform -tw-translate-y-1/2 -tw-translate-x-1/2 tw-w-fit"
+              >
+                <BaseIcon name="play" class="tw-w-6 tw-h-6" />
+              </BaseButton>
+              <div class="card__wrapper">
+                <div></div>
+                <div class="tw-grid tw-gap-1 tw-text-white">
+                  <div class="tw-text-h5">Камера 1</div>
                 </div>
               </div>
-            </SwiperSlide>
-          </Swiper>
+            </div>
+          </SwiperSlide>
+        </Swiper>
 
-          <button
-            @click="swiperCamera?.slidePrev()"
-            class="tw-absolute -tw-left-4 tw-top-1/2 tw-z-10 tw-transform -tw-translate-y-1/2 tw-rounded-lg tw-h-12 tw-w-12 tw-bg-text01/40 tw-backdrop-blur-sm tw-grid tw-place-content-center"
-          >
-            <BaseIcon name="back" class="tw-w-6 tw-h-6 tw-text-white" />
-          </button>
-          <button
-            @click="swiperCamera?.slideNext()"
-            class="tw-absolute -tw-right-4 tw-top-1/2 tw-z-10 tw-transform -tw-translate-y-1/2 tw-rounded-lg tw-h-12 tw-w-12 tw-bg-text01/40 tw-backdrop-blur-sm tw-grid tw-place-content-center"
-          >
-            <BaseIcon name="forward" class="tw-w-6 tw-h-6 tw-text-white" />
-          </button>
-        </div>
+        <button
+          @click="swiperCamera?.slidePrev()"
+          class="tw-absolute -tw-left-4 tw-top-1/2 tw-z-10 tw-transform -tw-translate-y-1/2 tw-rounded-lg tw-h-12 tw-w-12 tw-bg-text01/40 tw-backdrop-blur-sm tw-grid tw-place-content-center"
+        >
+          <BaseIcon name="back" class="tw-w-6 tw-h-6 tw-text-white" />
+        </button>
+        <button
+          @click="swiperCamera?.slideNext()"
+          class="tw-absolute -tw-right-4 tw-top-1/2 tw-z-10 tw-transform -tw-translate-y-1/2 tw-rounded-lg tw-h-12 tw-w-12 tw-bg-text01/40 tw-backdrop-blur-sm tw-grid tw-place-content-center"
+        >
+          <BaseIcon name="forward" class="tw-w-6 tw-h-6 tw-text-white" />
+        </button>
       </BaseTabsTabContentItem>
     </BaseTabsTabContent>
   </div>
