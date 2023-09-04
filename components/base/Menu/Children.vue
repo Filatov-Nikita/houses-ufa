2202428b38552674f05b343cef0ad57694ba36aa
Nikita Antonov <nikita45454@gmail.com>
2023-09-04 15:54:26 +0300

    feat: добавляет компонент меню

---
+++ components/base/Menu/Children.vue
@@ -0,0 +1,41 @@
+<template>
+  <transition
+    leave-active-class="animate__animated anim-300ms animate__fadeOut"
+    enter-active-class="animate__animated anim-300ms animate__fadeIn"
+  >
+    <div class="base-menu-children" v-if="showed">
+      <div class="base-menu-children__back">
+        <BaseMenuItem icon="back" label="Назад" @click="close" />
+      </div>
+      <slot />
+    </div>
+  </transition>
+</template>
+
+<script setup lang="ts">
+  interface Props {
+    close: () => void,
+    showed: boolean,
+  }
+
+  withDefaults(defineProps<Props>(), { showed: false });
+</script>
+
+<style scoped lang="scss">
+.base-menu-children {
+  position: absolute;
+  border-radius: 8px;
+  padding: 8px;
+  width: 100%;
+  min-height: 100%;
+  top: 0;
+  left: 0;
+  @apply tw-bg-white;
+
+  &__back {
+    padding-bottom: 8px;
+    margin-bottom: 8px;
+    border-bottom: 1px solid theme('colors.border00');
+  }
+}
+</style>
---
+++ components/base/Menu/Item.vue
@@ -0,0 +1,47 @@
+<template>
+  <component :is="tag" class="base-menu-item">
+    <div class="base-menu-item__icon">
+      <BaseIcon class="tw-w-full tw-h-full" :name="icon" />
+    </div>
+    <div class="base-menu-item__label">{{ label }}</div>
+  </component>
+</template>
+
+<script setup lang="ts">
+  interface Props {
+    tag?: string,
+    icon: string,
+    label: string
+  }
+
+  withDefaults(defineProps<Props>(), {
+    tag: 'button',
+  });
+</script>
+
+<style scoped lang="scss">
+  .base-menu-item {
+    display: flex;
+    align-items: center;
+    gap: 16px;
+    width: 100%;
+    padding: 8px 16px;
+    transition: background-color 0.3s;
+    border-radius: 8px;
+
+    &__icon {
+      width: 24px;
+      height: 24px;
+      @apply tw-text-icon;
+    }
+
+    &__label {
+      letter-spacing: -0.14px;
+      @apply tw-text-text00 tw-text-sm;
+    }
+
+    &:hover {
+      @apply tw-bg-base00;
+    }
+  }
+</style>
