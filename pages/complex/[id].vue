35476206a45e5ee1854b9603967066d363ca08d2
Nikita Antonov <nikita45454@gmail.com>
2023-10-06 21:41:09 +0300

    feat: прикручивает фильтр к карточке жк

--- pages/complex/[id].vue
+++ pages/complex/[id].vue
@@ -1,194 +1,188 @@
 <template>
-  <section class="head-section tw-flex tw-flex-col">
-    <div class="tw-container">
-      <BaseBanner actionLabel="Оставить заявку" theme="colored" class="tw-mb-3">
-        <p>
-          Оставить заявку. Оставьте свои контактные данные и мы свяжемся с вами
-        </p>
-      </BaseBanner>
-    </div>
-    <div class="tw-container tw-h-full">
-      <div class="head-section__wrapper">
-        <div>
-          <BreadCrumbs
-            :history-list="[{ to: '/', name: 'Главная' }, { name: 'ЖК' }]"
-            class="!tw-text-white tw-mb-4"
-          />
-          <h2 class="section__title title tw-mb-4 lg:tw-mb-2">
-            {{ complex?.name }}
-          </h2>
-          <h3>{{ complex?.location }}</h3>
-        </div>
-        <div class="content">
-          <BaseButton theme="white" class="tw-mb-4"
-            >Выбрать квартиру</BaseButton
-          >
-          <p class="">
-            Визуализация проекта предварительная, возможны изменения
+  <BaseWait :waiting="loadingComplex">
+    <template #loader>
+      <div class="tw-container tw-space-y-8">
+        <BaseSkeleton
+          class="tw-w-full tw-h-[300px]"
+          v-for="_i in 5"
+        />
+      </div>
+    </template>
+    <section class="head-section tw-flex tw-flex-col">
+      <div class="tw-container">
+        <BaseBanner actionLabel="Оставить заявку" theme="colored" class="tw-mb-3">
+          <p>
+            Оставить заявку. Оставьте свои контактные данные и мы свяжемся с вами
           </p>
+        </BaseBanner>
+      </div>
+      <div class="tw-container tw-h-full">
+        <div class="head-section__wrapper">
+          <div>
+            <BreadCrumbs
+              :history-list="[{ to: '/', name: 'Главная' }, { name: 'ЖК' }]"
+              class="!tw-text-white tw-mb-4"
+            />
+            <h2 class="section__title title tw-mb-4 lg:tw-mb-2">
+              {{ complex?.name }}
+            </h2>
+            <h3>{{ complex?.location }}</h3>
+          </div>
+          <div class="content">
+            <BaseButton theme="white" class="tw-mb-4"
+              >Выбрать квартиру</BaseButton
+            >
+            <p class="">
+              Визуализация проекта предварительная, возможны изменения
+            </p>
+          </div>
         </div>
       </div>
-    </div>
-  </section>
-  <section v-if="complex?.about">
-    <div class="tw-container">
-      <CardsAbout
-        v-bind="{
-          to: '/',
-          title: 'О жилом комплексе',
-          text: complex.about.description,
-          indicators: [
-            {
-              title: complex.about.feature_1_title,
-              subtitle: complex.about.feature_1_value,
-            },
-            {
-              title: complex.about.feature_2_title,
-              subtitle: complex.about.feature_2_value,
-            },
-            {
-              title: complex.about.feature_3_title,
-              subtitle: complex.about.feature_3_value,
-            },
-          ],
-        }"
-      />
-    </div>
-  </section>
-  <section>
-    <div class="tw-container">
-      <TabAdvantages />
-    </div>
-  </section>
-  <section>
-    <div class="tw-container">
-      <div class="section__top">
-        <h2 class="section__title">Генплан жилого комплекса {Название ЖК}</h2>
+    </section>
+    <section>
+      <div class="tw-container">
+        <ApartFilter />
       </div>
-      <GenplanZhk />
-    </div>
-  </section>
-  <section>
-    <div class="tw-container">
-      <div class="section__top">
-        <h2 class="section__title">Расположение и инфраструктура</h2>
+    </section>
+    <section v-if="complex?.about">
+      <div class="tw-container">
+        <CardsAbout
+          v-bind="{
+            to: '/',
+            title: 'О жилом комплексе',
+            text: complex.about.description,
+            indicators: [
+              {
+                title: complex.about.feature_1_title,
+                subtitle: complex.about.feature_1_value,
+              },
+              {
+                title: complex.about.feature_2_title,
+                subtitle: complex.about.feature_2_value,
+              },
+              {
+                title: complex.about.feature_3_title,
+                subtitle: complex.about.feature_3_value,
+              },
+            ],
+          }"
+        />
       </div>
-      <MapCategory />
-    </div>
-  </section>
-  <section>
-    <div class="tw-container">
-      <TabTypApartment />
-    </div>
-  </section>
-  <section>
-    <div class="tw-container">
-      <div class="section__top">
-        <h2 class="section__title">Как купить</h2>
+    </section>
+    <section>
+      <div class="tw-container">
+        <TabAdvantages />
       </div>
-      <div class="tw-grid lg:tw-grid-cols-3 tw-gap-4 lg:tw-gap-5">
-        <a
-          href="#"
-          class="tw-rounded-2xl tw-bg-white tw-grid lg:tw-flex tw-gap-6 lg:tw-gap-8 tw-px-4 tw-py-6 lg:tw-p-6"
-        >
-          <div
-            class="tw-w-12 tw-h-12 tw-bg-secondary tw-text-primary tw-rounded-lg tw-grid tw-place-content-center"
+    </section>
+    <section>
+      <div class="tw-container">
+        <div class="section__top">
+          <h2 class="section__title">Генплан жилого комплекса {{ complex?.name }}</h2>
+        </div>
+        <GenplanZhk />
+      </div>
+    </section>
+    <section>
+      <div class="tw-container">
+        <div class="section__top">
+          <h2 class="section__title">Расположение и инфраструктура</h2>
+        </div>
+        <MapCategory />
+      </div>
+    </section>
+    <section>
+      <div class="tw-container">
+        <TabTypApartment />
+      </div>
+    </section>
+    <section>
+      <div class="tw-container">
+        <div class="section__top">
+          <h2 class="section__title">Как купить</h2>
+        </div>
+        <div class="tw-grid lg:tw-grid-cols-3 tw-gap-4 lg:tw-gap-5">
+          <a
+            href="#"
+            class="tw-rounded-2xl tw-bg-white tw-grid lg:tw-flex tw-gap-6 lg:tw-gap-8 tw-px-4 tw-py-6 lg:tw-p-6"
           >
-            <BaseIcon class="tw-w-6 tw-h-6" name="wallet" />
-          </div>
+            <div
+              class="tw-w-12 tw-h-12 tw-bg-secondary tw-text-primary tw-rounded-lg tw-grid tw-place-content-center"
+            >
+              <BaseIcon class="tw-w-6 tw-h-6" name="wallet" />
+            </div>
 
-          <div class="tw-grid tw-gap-4">
-            <h4 class="tw-text-h6">Получить предложение</h4>
-            <p class="tw-text-body_m tw-text-text01">
-              Условия покупки и специальные предложения.
-            </p>
-          </div>
-        </a>
-        <a
-          href="#"
-          class="tw-rounded-2xl tw-bg-white tw-grid lg:tw-flex tw-gap-6 lg:tw-gap-8 tw-px-4 tw-py-6 lg:tw-p-6"
-        >
-          <div
-            class="tw-w-12 tw-h-12 tw-bg-secondary tw-text-primary tw-rounded-lg tw-grid tw-place-content-center"
+            <div class="tw-grid tw-gap-4">
+              <h4 class="tw-text-h6">Получить предложение</h4>
+              <p class="tw-text-body_m tw-text-text01">
+                Условия покупки и специальные предложения.
+              </p>
+            </div>
+          </a>
+          <a
+            href="#"
+            class="tw-rounded-2xl tw-bg-white tw-grid lg:tw-flex tw-gap-6 lg:tw-gap-8 tw-px-4 tw-py-6 lg:tw-p-6"
           >
-            <BaseIcon class="tw-w-6 tw-h-6" name="read" />
-          </div>
+            <div
+              class="tw-w-12 tw-h-12 tw-bg-secondary tw-text-primary tw-rounded-lg tw-grid tw-place-content-center"
+            >
+              <BaseIcon class="tw-w-6 tw-h-6" name="read" />
+            </div>
 
-          <div class="tw-grid tw-gap-4">
-            <h4 class="tw-text-h6">Купить онлайн</h4>
-            <p class="tw-text-body_m tw-text-text01">
-              Условия покупки и специальные предложения.
-            </p>
-          </div>
-        </a>
-        <a
-          href="#"
-          class="tw-rounded-2xl tw-bg-white tw-grid lg:tw-flex tw-gap-6 lg:tw-gap-8 tw-px-4 tw-py-6 lg:tw-p-6"
-        >
-          <div
-            class="tw-w-12 tw-h-12 tw-bg-secondary tw-text-primary tw-rounded-lg tw-grid tw-place-content-center"
+            <div class="tw-grid tw-gap-4">
+              <h4 class="tw-text-h6">Купить онлайн</h4>
+              <p class="tw-text-body_m tw-text-text01">
+                Условия покупки и специальные предложения.
+              </p>
+            </div>
+          </a>
+          <a
+            href="#"
+            class="tw-rounded-2xl tw-bg-white tw-grid lg:tw-flex tw-gap-6 lg:tw-gap-8 tw-px-4 tw-py-6 lg:tw-p-6"
           >
-            <BaseIcon class="tw-w-6 tw-h-6" name="rub" />
-          </div>
+            <div
+              class="tw-w-12 tw-h-12 tw-bg-secondary tw-text-primary tw-rounded-lg tw-grid tw-place-content-center"
+            >
+              <BaseIcon class="tw-w-6 tw-h-6" name="rub" />
+            </div>
 
-          <div class="tw-grid tw-gap-4">
-            <h4 class="tw-text-h6">Взять ипотеку</h4>
-            <p class="tw-text-body_m tw-text-text01">
-              Условия покупки и специальные предложения.
-            </p>
-          </div>
-        </a>
+            <div class="tw-grid tw-gap-4">
+              <h4 class="tw-text-h6">Взять ипотеку</h4>
+              <p class="tw-text-body_m tw-text-text01">
+                Условия покупки и специальные предложения.
+              </p>
+            </div>
+          </a>
+        </div>
       </div>
-    </div>
-  </section>
+    </section>
 
-  <section>
-    <div class="tw-container">
-      <div class="section__top">
-        <h2 class="section__title">Галерея</h2>
+    <section>
+      <div class="tw-container">
+        <div class="section__top">
+          <h2 class="section__title">Галерея</h2>
+        </div>
+        <TabGallery />
       </div>
-      <TabGallery />
-    </div>
-  </section>
-  <section>
-    <div class="tw-container">
-      <div class="section__top">
-        <h2 class="section__title">Ход строительства</h2>
+    </section>
+    <section>
+      <div class="tw-container">
+        <div class="section__top">
+          <h2 class="section__title">Ход строительства</h2>
+        </div>
+        <TabContructionProgress />
       </div>
-      <TabContructionProgress />
-    </div>
-  </section>
-  <section>
-    <div class="tw-container">
-      <FormCard1 />
-    </div>
-  </section>
+    </section>
+    <section>
+      <div class="tw-container">
+        <FormCard1 />
+      </div>
+    </section>
+  </BaseWait>
 </template>
 <script lang="ts" setup>
-interface Complex {
-  id: number
-  location: string
-  marketing_tag: null | string
-  name: string
-  about: {
-    id: number
-    description: string
-    feature_1_title: string
-    feature_1_value: string
-    feature_2_title: string
-    feature_2_value: string
-    feature_3_title: string
-    feature_3_value: string
-  }
-  seo: {
-    id: number
-    meta_description: string | null
-    meta_keywords: string | null
-    meta_title: string | null
-    text_description: string | null
-    text_title: string | null
-  }
-}
+import ApartFilter from '@/other-modules/apart-filter/index.vue';
+import { useComplexOne } from '@/stores/pages/complex-one';
+
 useHead({
   script: [
     {
@@ -197,17 +191,17 @@ useHead({
     },
   ],
 })
-const config = useRuntimeConfig()
-const route = useRoute()
-const complex = ref<Complex>()
-const getComplex = async () => {
-  const { data } = await useDataFetch(`estate/complexes/${route.params.id}`, {
-    baseURL: config.public.baseURL,
-  })
 
-  complex.value = data.value.data as Complex
-}
-getComplex()
+const complexOneStore = useComplexOne();
+const { setComplexId } = complexOneStore;
+
+const route = useRoute();
+
+const complexId = computed<number>(() => parseInt(route.params.id as string));
+const complex = computed(() => complexOneStore.complex?.data ?? null);
+const loadingComplex = computed(() => complexOneStore.loadingComplex);
+
+watch(complexId, (id) => setComplexId(id), { immediate: true });
 </script>
 <style lang="scss" scoped>
 .head-section {
