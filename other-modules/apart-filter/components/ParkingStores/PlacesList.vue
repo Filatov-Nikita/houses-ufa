81767e52ec624cdf136b93757b12aecf3b291b72
Nikita Antonov <nikita45454@gmail.com>
2023-10-08 21:42:30 +0300

    feat: добавляет компоненты списка кладовок/паркингов

---
+++ other-modules/apart-filter/components/ParkingStores/PlacesList.vue
@@ -0,0 +1,68 @@
+<template>
+  <BaseWait :waiting="loading">
+    <template #loader>
+      <div class="tw-pt-8">
+        <div class="tw-flex tw-flex-wrap -tw-m-[10px]">
+          <div
+            class="tw-w-1/2 tw-p-[10px]"
+            v-for="_i in 4"
+          >
+            <BaseSkeleton class="tw-w-full tw-min-h-[150px] tw-rounded-2xl" />
+          </div>
+        </div>
+      </div>
+    </template>
+    <div class="tw-py-10 tw-text-center" v-if="items.length === 0">
+      Ничего не найдено, измените значения фильтров.
+    </div>
+    <div class="places-area">
+      <div class="places-list">
+        <ListItem
+          class="places-list__item"
+          :place-name="placeName"
+          v-for="item in items"
+          v-bind="item"
+        />
+      </div>
+    </div>
+  </BaseWait>
+</template>
+
+<script setup lang="ts">
+  import ListItem from './ListItem.vue';
+
+  interface Item {
+    area_total: string,
+    price_total: string,
+    number: string
+  }
+
+  interface Props {
+    placeName: string,
+    loading: boolean,
+    items: Item[] | null
+  }
+
+  const props = defineProps<Props>();
+
+  const items = computed(() => props.items ?? []);
+</script>
+
+<style scoped lang="scss">
+  .places-list {
+    display: flex;
+    flex-wrap: wrap;
+    margin: -10px;
+
+    &__item {
+      width: calc(50% - 20px);
+      margin: 10px;
+    }
+  }
+
+  .places-area {
+    padding-top: 32px;
+    height: 500px;
+    overflow: auto;
+  }
+</style>
