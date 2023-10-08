88a8a364ad0e4276b955e041f6ea6d0d14477e0c
Nikita Antonov <nikita45454@gmail.com>
2023-10-08 21:45:09 +0300

    feat: соединяет компоненты фильтров паркинга/кладовок

---
+++ other-modules/apart-filter/components/ParkingStores/HeadFilter.vue
@@ -0,0 +1,175 @@
+<template>
+  <div class="head-filter">
+    <div class="head-filter__controls">
+      <FindedCount class="head-filter__count"
+        :total="total"
+        :cases="currentCases"
+      />
+      <div class="head-filter__btns">
+        <BaseTabsGroup theme="gray" v-model="currentView">
+          <BaseTabsGroupItem theme="gray" name="stores" :disabled="!canShowStores">
+            Кладовки
+          </BaseTabsGroupItem>
+          <BaseTabsGroupItem theme="gray" name="parking" :disabled="!canShowPark">
+            Паркинг
+          </BaseTabsGroupItem>
+        </BaseTabsGroup>
+        <BaseSelect
+          class="head-filter__sort"
+          theme="gray"
+          label=""
+          name="sort"
+          v-bind="selectProps(sortOptions)"
+          :modelValue="currentSort"
+          @update:modelValue="updateSort"
+        />
+      </div>
+    </div>
+    <BaseTabsTabContent v-bind="animTabs" v-model="currentView" keepAlive>
+      <BaseTabsTabContentItem name="stores" key="stores">
+        <PlacesList
+          placeName="Кладовка"
+          :loading="loadingStores"
+          :items="storeData"
+        />
+      </BaseTabsTabContentItem>
+      <BaseTabsTabContentItem name="parking" key="parking">
+        <PlacesList
+          placeName="Машиноместо"
+          :loading="loadingParking"
+          :items="parkData"
+        />
+      </BaseTabsTabContentItem>
+    </BaseTabsTabContent>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { useFilterStorehouse } from '../../store/filter-storehouse';
+  import { useFilterParking } from '../../store/filter-parking';
+  import { useFilterHead } from '../../store/filter-head';
+  import { useParkStorParams } from '../../store/filter-parkstor-params';
+  import { useComplexOne } from '@/stores/pages/complex-one';
+  import PlacesList from './PlacesList.vue';
+  import FindedCount from '../FindedCount.vue';
+  import { computed } from 'vue';
+
+  const animTabs = {
+    leaveClasses: '',
+    enterClasses: '',
+  }
+
+  type Cases = [string, string, string];
+  const casesStores: Cases = ['кладовка', 'кладовки', 'кладовок'];
+  const casesParkings: Cases = ['парковка', 'парковки', 'парковок'];
+
+  const filterStoreHouseStore = useFilterStorehouse();
+  const filterParkingStore = useFilterParking();
+  const filterParamsStore = useParkStorParams();
+  const filterHeadStore = useFilterHead();
+  const complexOneStore = useComplexOne();
+  const storehouses = computed(() => filterStoreHouseStore.storeshouses);
+  const parkings = computed(() => filterParkingStore.parkings);
+  const loadingParking = computed(() => filterParkingStore.loadingParkings);
+  const loadingStores = computed(() => filterStoreHouseStore.loadingStores);
+
+  const currentView = toRef(filterHeadStore, 'currentParkStoreFilter');
+
+  const total = computed(() => {
+    switch(currentView.value) {
+      case 'parking': return parkings.value?.meta.total ?? null;
+      case 'stores': return storehouses.value?.meta.total ?? null;
+    }
+  });
+
+  const currentCases = computed(() => {
+    switch(currentView.value) {
+      case 'parking': return casesParkings;
+      case 'stores': return casesStores;
+    }
+  });
+
+  const canShowPark = computed(() => complexOneStore.complex?.data.has_parkings === true);
+  const canShowStores = computed(() => complexOneStore.complex?.data.has_storehouses === true);
+
+  const parkData = computed(
+    () => filterParkingStore.parkings?.data.map(item => toItem(item)) ?? null
+  );
+
+  const storeData = computed(
+    () => filterStoreHouseStore.storeshouses?.data.map(item => toItem(item)) ?? null
+  );
+
+  function toItem<T extends Itemable>(item: T): Itemable {
+    const { price_total, area_total, number } = item;
+    return { price_total, area_total, number }
+  }
+
+  type Itemable = {
+    area_total: string,
+    price_total: string,
+    number: string
+  };
+
+  const sortOptions = [
+    { label: 'Сначала дешевле', order_by_direction: 'asc', order_by_field: 'price_total' },
+    { label: 'Сначала дороже', order_by_direction: 'desc', order_by_field: 'price_total' },
+  ] as const;
+
+  const currentSort = computed(() => {
+    return sortOptions.find(opt => {
+      return opt.order_by_direction === filterParamsStore.filterParams.order_by_direction
+      && opt.order_by_field === filterParamsStore.filterParams.order_by_field
+    }) ?? null;
+  });
+
+  function updateSort(opt: typeof sortOptions[number]) {
+    filterParamsStore.filterParams.order_by_direction = opt.order_by_direction;
+    filterParamsStore.filterParams.order_by_field = opt.order_by_field;
+  }
+
+  function selectProps(options: typeof sortOptions) {
+    return {
+      'drop-down-props': {
+        getLabel: (opt: any) => opt.label,
+        isActive: (opt: any, v: any) => {
+          return opt.order_by_direction === v?.order_by_direction
+          && opt.order_by_field === v?.order_by_field;
+        },
+        options,
+      },
+      'display-props': {
+        getLabel: (v: any) => v?.label || 'сортировка',
+      }
+    };
+  }
+</script>
+
+<style scoped lang="scss">
+.head-filter {
+  @apply tw-rounded-2xl tw-bg-white tw-p-6;
+
+  &__controls {
+    display: flex;
+    flex-wrap: wrap;
+    column-gap: 30px;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  &__count {
+    flex-basis: 270px;
+    @apply tw-text-lg tw-text-text00;
+  }
+
+  &__btns {
+    display: flex;
+    flex-wrap: wrap;
+    column-gap: 16px;
+  }
+
+  &__sort {
+    width: 250px;
+  }
+}
+</style>
