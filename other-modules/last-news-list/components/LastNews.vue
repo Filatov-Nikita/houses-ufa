eb1b55d61055e2fe90fcf3e379db059a6cec0444
Nikita Antonov <nikita45454@gmail.com>
2023-10-09 15:19:12 +0300

    feat: создает фича компонент списка последних новостей

---
+++ other-modules/last-news-list/components/LastNews.vue
@@ -0,0 +1,10 @@
+<template>
+  <LastNewsList :loading="store.loading" :news="store.lastNews" />
+</template>
+
+<script setup lang="ts">
+  import LastNewsList from './ListItems.vue';
+  import { useLastNewsListStore } from '../store';
+
+  const store = useLastNewsListStore();
+</script>
