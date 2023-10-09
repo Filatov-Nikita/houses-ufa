bd7ea8f4a53ff772df20765b3827e265d3213895
Nikita Antonov <nikita45454@gmail.com>
2023-10-09 21:17:29 +0300

    feat: добавляет тулбар в список новостей

---
+++ other-modules/articles-filter/components/Toolbar.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="toolbar">
+    <div class="toolbar__tabs">
+      <BaseTabsGroup v-model="filter.currentTab">
+        <BaseTabsGroupItem name="tiding" theme="white">
+          Новости
+        </BaseTabsGroupItem>
+        <BaseTabsGroupItem name="mass_media" theme="white">
+          СМИ о нас
+        </BaseTabsGroupItem>
+      </BaseTabsGroup>
+    </div>
+    <div class="toolbar__params">
+      <SelectYears />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import SelectYears from './SelectYears.vue';
+  import { useArticlesFilter } from '../store';
+
+  const filter = useArticlesFilter();
+</script>
+
+<style scoped lang="scss">
+  .toolbar {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    align-items: center;
+    gap: 32px;
+
+    &__params {
+      flex-basis: 160px;
+    }
+  }
+</style>
