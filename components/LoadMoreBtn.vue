c852c8c9173190e935ef3c509dd584cd67f3f62f
Nikita Antonov <nikita45454@gmail.com>
2023-10-09 21:13:35 +0300

    feat: добавляет loading more кнопку

---
+++ components/LoadMoreBtn.vue
@@ -0,0 +1,14 @@
+<template>
+  <BaseButton class="tw-w-full" theme="white">
+    Показать ещё {{ loaded }} из {{ total }}
+  </BaseButton>
+</template>
+
+<script setup lang="ts">
+  interface Props {
+    total: number,
+    loaded: number
+  }
+
+  defineProps<Props>();
+</script>
