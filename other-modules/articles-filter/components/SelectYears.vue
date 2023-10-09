48e922c80149c6ae84a7759bef1a75021e48f8bd
Nikita Antonov <nikita45454@gmail.com>
2023-10-09 21:17:03 +0300

    feat: добавляет селект выбора года в фильтр новостей

---
+++ other-modules/articles-filter/components/SelectYears.vue
@@ -0,0 +1,61 @@
+<template>
+  <BaseSelect
+    theme="white"
+    label=""
+    name="date"
+    v-bind="selectProps(options)"
+    :model-value="current"
+    @update:model-value="updateValue"
+  />
+</template>
+
+<script setup lang="ts">
+  import { useArticlesFilter } from '../store';
+
+  const filter = useArticlesFilter();
+
+  function getOptions() {
+    const today = new Date();
+    const curYear = today.getFullYear();
+    const list: number[] = [];
+
+    for(let i = 0; i <= 5; i++) {
+      list.push(curYear - i);
+    }
+
+    return list;
+  }
+
+  function getFullYear(year: number) {
+    return `${year}-01-01`;
+  }
+
+  const options = computed(() => {
+    return [
+      { label: 'Все года' , value: null },
+      ...getOptions()
+      .map( year => ({ label: year.toString(), value: getFullYear(year) }) ),
+    ];
+  });
+
+  const current = computed(() => {
+    return options.value.find((opts) => opts.value === filter.params.date) ?? null
+  });
+
+  function updateValue(val: { label: string, value: string | null } | null) {
+    filter.params.date = val?.value ?? null;
+  }
+
+  function selectProps<T extends { label: string, value: string | null }>(options: T[]) {
+    return {
+      'drop-down-props': {
+        getLabel: (opt: T) => opt.label,
+        isActive: (opt: T, v: T | null) => opt.value === v?.value,
+        options,
+      },
+      'display-props': {
+        getLabel: (v: T | null) => v?.label || 'Все года',
+      }
+    };
+  };
+</script>
