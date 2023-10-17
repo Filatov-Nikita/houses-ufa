7c58c607c11cccc166edf25b2c7ef9878120f1bc
Nikita Antonov <nikita45454@gmail.com>
2023-10-17 23:19:32 +0300

    fix: переносит стили в список главного фильтра

--- other-modules/main-filter/components/Content.vue
+++ other-modules/main-filter/components/Content.vue
@@ -9,15 +9,3 @@
   import ListControl from './ListControl.vue';
   import EstateList from './EstateList.vue';
 </script>
-
-<style scoped lang="scss">
-  .filter-estate-list {
-    margin: -16px -10px;
-    @apply tw-flex tw-flex-wrap;
-
-    &__card {
-      width: calc(100% / 4 - 20px);
-      margin: 16px 10px;
-    }
-  }
-</style>
--- other-modules/main-filter/components/EstateList.vue
+++ other-modules/main-filter/components/EstateList.vue
@@ -81,3 +81,14 @@
     }
   });
 </script>
+<style scoped lang="scss">
+  .filter-estate-list {
+    margin: -16px -10px;
+    @apply tw-flex tw-flex-wrap;
+
+    &__card {
+      width: calc(100% / 4 - 20px);
+      margin: 16px 10px;
+    }
+  }
+</style>
