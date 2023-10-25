c68c6cc63fcf9e032446c822e0c6e5a852ed7a78
Nikita Antonov <nikita45454@gmail.com>
2023-10-25 17:09:41 +0500

    feat: добавляет слайдер для карточек недвижки

---
+++ components/Estate/Image/Slider.vue
@@ -0,0 +1,59 @@
+<template>
+  <Swiper class="estate-image-slider" v-bind="swiperProps">
+    <SwiperSlide class="estate-image-slider__slide" v-for="image in images">
+      <img
+        class="estate-image-slider__img"
+        :src="image"
+      />
+    </SwiperSlide>
+    <NavBtns />
+    <div class="swiper-pagination estate-image-slider__pag"></div>
+  </Swiper>
+</template>
+
+<script setup lang="ts">
+  import NavBtns from '@/components/sliders/NavBtns.vue';
+
+  interface Props {
+    images: Array<string>
+  }
+
+  defineProps<Props>();
+
+  const swiperProps = {
+    modules: [SwiperPagination, SwiperNavigation],
+    spaceBetween: 24,
+    pagination: {
+      type: 'bullets',
+      el: '.swiper-pagination',
+      dynamicBullets: true,
+    } as const,
+    navigation: {
+      nextEl: '.swiper-next',
+      prevEl: '.swiper-prev',
+    } as const,
+  };
+</script>
+
+<style scoped lang="scss">
+  .estate-image-slider {
+    position: relative;
+
+    &__pag {
+      left: auto !important;
+      right: -16px !important;
+    }
+
+    &__slide {
+      position: relative;
+    }
+
+    &__img {
+      border-radius: 16px;
+      object-fit: cover;
+      position: absolute;
+      width: 100%;
+      height: 100%;
+    }
+  }
+</style>
---
+++ components/sliders/NavBtns.vue
@@ -0,0 +1,47 @@
+<template>
+  <button
+    class="swiper-prev swiper-nav swiper-nav--left"
+  >
+    <BaseIcon class="swiper-nav__icon" name="back" />
+  </button>
+  <button
+    class="swiper-next swiper-nav swiper-nav--right"
+  >
+    <BaseIcon class="swiper-nav__icon" name="forward" />
+  </button>
+</template>
+
+<script setup lang="ts">
+
+</script>
+<style scoped lang="scss">
+  .swiper-nav {
+    width: 40px;
+    height: 40px;
+    border-radius: 8px;
+    padding: 8px;
+    backdrop-filter: blur(4px);
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    z-index: 10;
+    @apply tw-bg-text01/40 tw-text-white;
+
+    &:disabled {
+      display: none;
+    }
+
+    &--left {
+      left: 24px;
+    }
+
+    &--right {
+      right: 24px;
+    }
+
+    &__icon {
+      width: 24px;
+      height: 24px;
+    }
+  }
+</style>
