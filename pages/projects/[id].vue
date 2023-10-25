88a762732dfc98cb43db4330590f01027bd2d54d
Nikita Antonov <nikita45454@gmail.com>
2023-10-25 23:29:31 +0500

    feat: страница проекта - встраивает "Остались вопросы"

--- pages/projects/[id].vue
+++ pages/projects/[id].vue
@@ -1,11 +1,13 @@
 <template>
-  <div>
-    <div class="wrapper">
-      <ProjectCard />
-    </div>
+  <div class="wrapper">
+    <ProjectCard />
+  </div>
+  <div class="wrapper">
+    <QuestionForm />
   </div>
 </template>
 
 <script setup lang="ts">
   import ProjectCard from '@/other-modules/project-card/index.vue';
+  import QuestionForm from '@/other-modules/question-form/index.vue';
 </script>
