99fd729876b4670c173817ff8380cbf94887db63
Nikita Antonov <nikita45454@gmail.com>
2023-10-25 23:27:56 +0500

    feat: добавляет форму "Получить консультацию"

---
+++ other-modules/consult-form/components/Form.vue
@@ -0,0 +1,73 @@
+<template>
+  <Form class="consult-form">
+    <BaseInput class="consult-form__item" rules="required" label="Имя" name="name" placeholder="Иван" />
+    <div class="consult-form__section consult-form__item">
+      <BaseInput
+        class="consult-form__column"
+        rules="required"
+        label="Телефон"
+        name="phone"
+        placeholder="+7 (XXX) XXX XX XX"
+        maska="+7 (###) ### ## ##"
+      />
+      <BaseInput
+        class="consult-form__column"
+        rules="required|email"
+        label="E-mail"
+        name="email"
+        placeholder="name@gmail.com"
+      />
+    </div>
+    <BaseInput
+      class="consult-form__item consult-form__item"
+      label="Сообщение"
+      name="text"
+      placeholder="У меня есть вопрос..."
+    />
+    <div class="consult-form__section consult-form__item">
+      <p class="consult-form__column consult-form__perc">
+        Нажимая кнопку, вы соглашаетесь с&nbsp;<a href="#">условиями обработки персональных данных</a>
+      </p>
+      <BaseButton class="consult-form__column" type="submit">Получить консультацию</BaseButton>
+    </div>
+  </Form>
+</template>
+
+<script setup lang="ts">
+  import { Form } from 'vee-validate';
+</script>
+
+<style scoped lang="scss">
+  .consult-form {
+    &__column {
+      margin: 0 10px;
+      width: calc(50% - 20px);
+    }
+
+    &__section {
+      display: flex;
+      flex-wrap: wrap;
+      margin: 0 -10px;
+    }
+
+    &__perc {
+      @apply tw-text-sm tw-text-text02;
+
+      a {
+        @apply tw-text-primary;
+      }
+    }
+
+    &__bottom {
+      margin-top: 32px;
+    }
+
+    &__item + &__item {
+      margin-top: 20px;
+    }
+
+    &__item:last-child {
+      margin-top: 32px;
+    }
+  }
+</style>
---
+++ other-modules/consult-form/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <BaseModal v-model="showed" v-slot="{ hide }">
+    <BaseModalCard>
+      <div class="consult-header">
+        <div class="consult-header__text">
+          <p class="consult-header__title">Получить консультацию</p>
+          <p class="consult-header__subtitle">
+            Оставьте свои контактные данные и мы свяжемся с вами
+          </p>
+        </div>
+        <BtnsActionsBase icon="close" @click="hide" />
+      </div>
+      <Form />
+    </BaseModalCard>
+  </BaseModal>
+</template>
+
+<script setup lang="ts">
+  import Form from './components/Form.vue';
+  import { useSyncProps } from '@/composables/useSyncProp';
+
+  const props = defineProps<{
+    showed: boolean
+  }>();
+
+  defineEmits<{ (event: 'update:showed', val: boolean): void }>();
+
+  const showed = useSyncProps(props, 'showed');
+</script>
+
+<style scoped lang="scss">
+  .consult-header {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    margin-bottom: 32px;
+
+    &__title {
+      font-size: 32px;
+      line-height: 1.25;
+      margin-bottom: 8px;
+    }
+
+    &__subtitle {
+      @apply tw-text-text02 tw-text-base;
+    }
+  }
+</style>
