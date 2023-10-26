d2f0e8ac3dde775dac1d654c13ac80cb2aa5f103
Nikita Antonov <nikita45454@gmail.com>
2023-10-26 20:52:15 +0500

    feat: встраивает список проектов на страницу проекта

--- pages/projects/[id].vue
+++ pages/projects/[id].vue
@@ -1,13 +1,24 @@
 <template>
-  <div class="wrapper">
-    <ProjectCard />
-  </div>
-  <div class="wrapper">
-    <QuestionForm />
-  </div>
+  <section>
+    <div class="wrapper">
+      <ProjectCard />
+    </div>
+  </section>
+  <section>
+    <div class="wrapper">
+      <h2 class="section__title section__top">Другие проекты коттеджей</h2>
+      <ProjectList disabledTabs />
+    </div>
+  </section>
+  <section>
+    <div class="wrapper">
+      <QuestionForm  />
+    </div>
+  </section>
 </template>
 
 <script setup lang="ts">
   import ProjectCard from '@/other-modules/project-card/index.vue';
+  import ProjectList from '@/other-modules/project-list/index.vue';
   import QuestionForm from '@/other-modules/question-form/index.vue';
 </script>
