bf2ad509fac4a64d0bdc664ba103854d0197e5db
Nikita Antonov <nikita45454@gmail.com>
2023-10-29 21:26:24 +0500

    feat: добавляет компоненты способа покупки

---
+++ components/Estate/PurchaseMethods/Item.vue
@@ -0,0 +1,59 @@
+<template>
+  <article class="buy-item">
+    <div class="buy-item__icon">
+      <BaseIcon class="buy-item__svg" :name="icon" />
+    </div>
+    <div class="buy-item__right">
+      <h3 class="buy-item__title">{{ title }}</h3>
+      <p class="buy-item__text">{{ text }}</p>
+    </div>
+  </article>
+</template>
+
+<script setup lang="ts">
+  interface Props {
+    title: string,
+    text: string,
+    icon: string,
+  }
+
+  defineProps<Props>();
+</script>
+
+<style scoped lang="scss">
+  .buy-item {
+    padding: 24px;
+    border-radius: 16px;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 24px;
+    @apply tw-bg-white;
+
+    &__title {
+      margin-bottom: 16px;
+      @apply tw-text-text00 tw-text-xl;
+    }
+
+    &__text {
+      @apply tw-text-text01 tw-text-base;
+    }
+
+    &__icon {
+      border-radius: 8px;
+      padding: 8px;
+      width: 48px;
+      height: 48px;
+      @apply tw-bg-base00 tw-text-primary;
+    }
+
+    &__svg {
+      width: 100%;
+      height: 100%;
+    }
+
+    &__right {
+      flex-basis: 280px;
+      flex-grow: 1;
+    }
+  }
+</style>
---
+++ components/Estate/PurchaseMethods/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="purch-methods">
+    <EstatePurchaseMethodsItem
+      class="purch-methods__item"
+      v-for="item in items"
+      :key="item.title"
+      v-bind="item"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+  const items = [
+    {
+      title: 'Получить предложение',
+      text: 'Условия покупки и специальные предложения.',
+      icon: 'wallet',
+    },
+    {
+      title: 'Купить онлайн',
+      text: 'Условия покупки и специальные предложения.',
+      icon: 'read',
+    },
+    {
+      title: 'Взять ипотеку',
+      text: 'Условия покупки и специальные предложения.',
+      icon: 'rub',
+    },
+  ];
+</script>
+
+<style scoped lang="scss">
+  .purch-methods {
+    display: flex;
+    flex-wrap: wrap;
+    margin: -10px;
+
+    &__item {
+      width: calc(100% / 12 * 4 - 20px);
+      margin: 10px;
+    }
+  }
+</style>
