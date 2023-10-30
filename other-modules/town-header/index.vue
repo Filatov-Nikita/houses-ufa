b39c131c49079f7ba0efded9d04563c65bf690c3
Nikita Antonov <nikita45454@gmail.com>
2023-10-30 17:07:31 +0500

    feat: добавляет модуль шапки для таунов

---
+++ other-modules/town-header/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <EstateBlockHeader
+    :name="townStore.town?.data.name ?? ''"
+    :location="townStore.town?.data.location ?? ''"
+    actionTitle="Выбрать дом"
+    caption="Визуализация проекта предварительная, возможны изменения"
+    @action="showMainFilter"
+  />
+</template>
+
+<script setup lang="ts">
+  import { useTownOne } from '@/stores/pages/town-one';
+
+  const router = useRouter();
+  const route = useRoute();
+  const townStore = useTownOne();
+
+  function showMainFilter() {
+    router.push({
+      path: '/main-filter',
+      query: { filterType: 'towns', object_id: +route.params.id },
+    });
+  }
+</script>
