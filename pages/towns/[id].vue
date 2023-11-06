a428d32e90b3dffd6c6b233f98d9747273c9019a
Nikita Antonov <nikita45454@gmail.com>
2023-11-06 12:31:35 +0500

    feat: встраивает модули в карточку коттеджа

--- pages/towns/[id].vue
+++ pages/towns/[id].vue
@@ -1,48 +1,27 @@
 <template>
+  <div>
+    <div class="wrapper">
+      <TownCard />
+    </div>
+  </div>
   <section>
-    <div class="tw-container">
-      <CardsPlot v-if="houseBuilding" />
-      <CardsHomeBuilding v-else :is-buildinghouse="true" />
+    <div class="wrapper">
+      <CreditCalc />
     </div>
   </section>
   <section>
-    <div class="tw-container">
-      <div class="section__top">
-        <h2 class="section__title">Ипотека</h2>
-      </div>
-      <CardsIpotekaForm />
-    </div>
-  </section>
-  <section>
-    <div class="tw-container">
-      <div class="section__top">
-        <h2 class="section__title">Похожие дома в этом посёлке</h2>
-      </div>
-      <div
-        class="tw-grid lg:tw-grid-cols-3 xl:tw-grid-cols-4 tw-gap-4 lg:tw-gap-5"
-      >
-        <CardsApartmentMini
-          v-for="n in 4"
-          v-bind="{
-            title: 'Брайтон Плюс',
-            type: 'Коттедж',
-            square: '409.52',
-            src: ['/images/img/test.png', '/images/img/test.png'],
-          }"
-        />
-      </div>
-      <BaseButton theme="white" class="tw-w-full tw-mt-6 xl:tw-mt-8">
-        Показать ещё 8 из 128
-      </BaseButton>
-    </div>
-  </section>
-  <section>
-    <div class="tw-container">
-      <FormCard1 />
+    <div class="wrapper">
+      <QuestionForm />
     </div>
   </section>
 </template>
-<script lang="ts" setup>
-const houseBuilding = ref(false)
+
+<script setup lang="ts">
+  import TownCard from '@/other-modules/town-card/index.vue';
+  import CreditCalc from '@/other-modules/credit-program-calc/index.vue';
+  import QuestionForm from '@/other-modules/question-form/index.vue';
 </script>
-<style lang="scss" scoped></style>
+
+<style scoped lang="scss">
+
+</style>
