dc78f4c67deda52623420b692593ca2a0111051e
Nikita Antonov <nikita45454@gmail.com>
2023-11-06 12:31:19 +0500

    feat: встраивает модули в карточки квартиры

--- pages/apartments/[id].vue
+++ pages/apartments/[id].vue
@@ -1,45 +1,26 @@
 <template>
-  <section>
-    <div class="tw-container">
-
-      <CardsFlat />
+  <div>
+    <div class="wrapper">
+      <FlatCard />
     </div>
-  </section>
+  </div>
   <section>
-    <div class="tw-container">
-      <div
-        class="tw-grid lg:tw-grid-cols-3 xl:tw-grid-cols-4 tw-gap-4 lg:tw-gap-5"
-      >
-        <CardsApartmentMini
-          v-for="n in 4"
-          v-bind="{
-            title: 'Брайтон Плюс',
-            type: 'Коттедж',
-            square: '409.52',
-            src: ['/images/img/test.png', '/images/img/test.png'],
-          }"
-        />
-      </div>
-      <BaseButton theme="white" class="tw-w-full tw-mt-6 xl:tw-mt-8">
-        Показать ещё 8 из 128
-      </BaseButton>
-    </div>
-  </section>
-  <section>
-    <div class="tw-container">
+    <div class="wrapper">
       <div class="section__top">
         <h2 class="section__title">Ипотека</h2>
       </div>
-      <CardsIpotekaForm />
+      <CreditCalc />
     </div>
   </section>
   <section>
-    <div class="tw-container">
-      <FormCard1 />
+    <div class="wrapper">
+      <QuestionForm />
     </div>
   </section>
 </template>
-<script lang="ts" setup>
-const houseBuilding = ref(false)
+
+<script setup lang="ts">
+  import QuestionForm from '@/other-modules/question-form/index.vue';
+  import CreditCalc from '@/other-modules/credit-program-calc/index.vue';
+  import FlatCard from '@/other-modules/flat-card/index.vue';
 </script>
-<style lang="scss" scoped></style>
