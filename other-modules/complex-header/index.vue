03bd0acafa232fcecc849431f0591cd0ef007dc3
Nikita Antonov <nikita45454@gmail.com>
2023-11-06 12:28:50 +0500

    feat: добавляет модуль шапки комплекса

---
+++ other-modules/complex-header/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <EstateBlockHeader
+    :name="complexStore.complex?.data.name ?? ''"
+    :location="complexStore.complex?.data.location ?? ''"
+    actionTitle="Выбрать квартиру"
+    caption="Визуализация проекта предварительная, возможны изменения"
+    @action="showMainFilter"
+  />
+</template>
+
+<script setup lang="ts">
+  import { useComplexOne } from '@/stores/pages/complex-one';
+
+  const router = useRouter();
+  const route = useRoute();
+  const complexStore = useComplexOne();
+
+  function showMainFilter() {
+    router.push({
+      path: '/main-filter',
+      query: { filterType: 'flats' },
+    });
+  }
+</script>
