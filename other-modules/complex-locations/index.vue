9c1db8dc9fe2476d9c9220879eb8ddb55893084f
Nikita Antonov <nikita45454@gmail.com>
2023-11-12 21:06:55 +0500

    feat: соединяет карту локаций и сайдбар в один модуль

---
+++ other-modules/complex-locations/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="complex-locations">
+    <Sidebar class="complex-locations__sidebar" />
+    <Map class="complex-locations__map" v-if="store.data && complex.complex" />
+  </div>
+</template>
+
+<script setup lang="ts">
+  import Sidebar from './components/SideBar.vue';
+  import Map from './components/Map.vue';
+  import { useComplexLocations } from './store';
+  import { useComplexOne } from '@/stores/pages/complex-one';
+
+  const store = useComplexLocations();
+  const route = useRoute();
+  const complex = useComplexOne();
+
+  store.setComplexId(+route.params.id);
+
+  await useLazyAsyncData(() => store.show());
+</script>
+
+
+<style scoped lang="scss">
+  .complex-locations {
+    position: relative;
+
+    &__sidebar {
+      width: 305px;
+      position: absolute;
+      left: 40px;
+      top: 40px;
+      z-index: 100;
+    }
+
+    &__map {
+      height: 608px;
+    }
+  }
+</style>
