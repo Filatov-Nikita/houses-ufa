828581c1e49319d58287c5c13c309103b43c3bf9
Nikita Antonov <nikita45454@gmail.com>
2023-11-22 20:25:39 +0500

    убирает лишние обертки в апп компоненте

--- app.vue
+++ app.vue
@@ -1,12 +1,11 @@
 <template>
-  <div>
-    <ClientOnly>
-      <BaseNotify />
-    </ClientOnly>
-    <NuxtLayout>
-      <NuxtPage />
-    </NuxtLayout>
-  </div>
+  <ClientOnly>
+    <BaseNotify />
+  </ClientOnly>
+
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
 <style lang="scss">
   @use './assets/css/main.scss';
