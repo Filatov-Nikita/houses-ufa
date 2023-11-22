f7f4119dda9ce4bbcba40c4881d02b3108c3efc3
Nikita Antonov <nikita45454@gmail.com>
2023-11-22 20:28:17 +0500

    refactor: layout - удаляет лишнии обертки, стилевые правки, встраивает новые футер и хедеры

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,32 +1,27 @@
 <template>
-  <div>
-    <HeaderMain />
-    <main class="tw-flex tw-flex-col tw-gap-12 lg:tw-gap-20 xl:tw-gap-24">
-      <slot />
-    </main>
-    <FooterMain class="tw-mt-12 lg:tw-mt-20 xl:tw-mt-24" />
-    <ModalCallback :model-value="showedCallback" @update:model-value="updateCallback"/>
+  <div class="wrapper tw-py-4">
+    <PublicHeader />
+  </div>
+
+  <main>
+    <slot />
+  </main>
+
+  <div class="wrapper">
+    <PublicFooter />
   </div>
 </template>
 
 <script setup lang="ts">
-import HeaderMain from '../components/layout/HeaderMain.vue'
-import FooterMain from '../components/layout/FooterMain.vue'
-import ModalCallback from '../components/modals/Form.vue'
-import { useAppStore } from '@/stores/app';
+import PublicHeader from '@/other-modules/public-header/index.vue';
+import PublicFooter from '@/other-modules/public-footer/index.vue';
 import { useComplexesStore } from '@/stores/complexes';
 import { useTownsStore } from '@/stores/towns';
 
 const complexesStore = useComplexesStore();
 const townsStore = useTownsStore();
-const appStore = useAppStore();
-const showedCallback = computed(() => appStore.showedCallback);
 
 useAsyncData(() => {
   return Promise.all([ complexesStore.show(), townsStore.show() ]);
 });
-
-function updateCallback(val: boolean) {
-  appStore.showedCallback = val;
-}
 </script>
