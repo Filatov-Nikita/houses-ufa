10527ef69398cb62f8c98c2d1ba7a79e2fcfa2de
Nikita Antonov <nikita45454@gmail.com>
2023-11-23 22:30:41 +0500

    устанавливает ссылки для футера и хедера

--- stores/menu/composables/useNavMenu.ts
+++ stores/menu/composables/useNavMenu.ts
@@ -3,7 +3,59 @@ import type { Section } from '../types';
 type Sections = Record<string, Section>;
 
 export function useNavMenu(sectionList: ComputedRef<Sections>) {
-  const sections: Sections = reactive({});
+  const sections: Sections = reactive({
+    about: {
+      label: 'О компании',
+      links: [
+        {
+          label: 'О нас',
+          to: '/about',
+        },
+        {
+          label: 'История компании',
+          to: '/about#history',
+        },
+        {
+          label: 'Команда',
+          to: '/about#team',
+        },
+        {
+          label: 'Работа в компании',
+          to: '/about#work',
+        },
+      ],
+    },
+    press: {
+      label: 'Пресс-центр',
+      links: [
+        {
+          label: 'Пресс-центр',
+          to: '/articles',
+        },
+        {
+          label: 'СМИ о нас',
+          to: '/articles',
+        },
+        {
+          label: 'Вопросы и ответы',
+          to: '/faq',
+        },
+      ],
+    },
+    contacts: {
+      label: 'Контакты',
+      links: [
+        {
+          label: 'Офисы',
+          to: '/contacts',
+        },
+        {
+          label: 'Сервисные службы',
+          to: '/contacts',
+        },
+      ],
+    },
+  });
 
   const items = computed<Sections>(() => {
     return Object.assign({}, sectionList.value, sections);
