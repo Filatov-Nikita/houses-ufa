dcd45b512b263c24e606b4157d70ec8a5ba575f3
Nikita Antonov <nikita45454@gmail.com>
2023-11-28 13:52:24 +0500

    добавляет слот для текста в модуль PageInfo

--- components/page/Info.vue
+++ components/page/Info.vue
@@ -3,7 +3,8 @@
     <div class="page-info__left">
       <BaseBreadcrumbs class="page-info__bread" />
       <h1 class="page-info__title">{{ title }}</h1>
-      <p class="page-info__text" v-html="text"></p>
+      <slot name="text" v-if="$slots.text" />
+      <p v-else-if="text" class="page-info__text" v-html="text"></p>
     </div>
     <svg class="page-info__decor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 660 304" fill="none" preserveAspectRatio="none">
       <path d="M120.86 440L763 440L763 6.10352e-05L120.86 4.89746e-06C50.0992 -1.28862e-06 14.719 -3.48992e-05 3.17374 22.4597C-8.37149 44.9194 12.2916 73.5496 53.6177 130.81L83.1799 171.771C99.9812 195.05 108.382 206.69 108.382 220C108.382 233.311 99.9813 244.95 83.1804 268.229L83.1798 268.23L53.6181 309.19C12.2918 366.45 -8.37133 395.08 3.17383 417.54C14.7191 440 50.0992 440 120.86 440Z" fill="#F0F0F0"/>
@@ -15,7 +16,7 @@
 <script setup lang="ts">
   defineProps<{
     title: string,
-    text: string,
+    text?: string,
   }>();
 </script>
 
