31fa97fc77c76dd15a07ef8af9ce9634156b176a
Nikita Antonov <nikita45454@gmail.com>
2023-11-30 17:13:07 +0500

    правки по контенту соц. сетей и номеров для контактов

--- stores/contacts/composables/useSocial.ts
+++ stores/contacts/composables/useSocial.ts
@@ -2,5 +2,7 @@ export function useSocial() {
   return {
     vk: 'https://vk.com/zhiloikvartal',
     ok: 'https://ok.ru/group/52485575540911',
+    telegram: 'https://t.me/zhiloikvartal',
+    whatsapp: 'https://wa.clck.bar/79872500270',
   };
 }
