4f5f02698c033514b059f93787ace0d2ac3f11c6
Nikita Antonov <nikita45454@gmail.com>
2023-12-12 19:36:45 +0500

    заменяет markquiz

--- scripts/marquiz.ts
+++ scripts/marquiz.ts
@@ -20,5 +20,5 @@ export const body1 = `
 `;
 
 export const body2 = `
-  (function(t, p) {window.Marquiz ? Marquiz.add([t, p]) : document.addEventListener('marquizLoaded', function() {Marquiz.add([t, p])})})('Widget', {id: '6572a2c52a05bd00254ca5ce', position: 'right', delay: 30, autoOpen: 45, disableIfClosed: true})
+  (function(t, p) {window.Marquiz ? Marquiz.add([t, p]) : document.addEventListener('marquizLoaded', function() {Marquiz.add([t, p])})})('Widget', {id: '6572a2c52a05bd00254ca5ce', position: 'right', delay: 20, autoOpen: 40})
 `;
