519ecd5b060502ffa1f883b53033a8dfb6fee469
Nikita Antonov <nikita45454@gmail.com>
2023-12-15 15:36:20 +0500

    добавляет мидлвару для инициализации избранного

---
+++ middleware/favorite.global.ts
@@ -0,0 +1,14 @@
+import { useAuthStore } from '@/stores/auth';
+import { useFavoritesStore } from '@/stores/favorites';
+
+export default defineNuxtRouteMiddleware(async (to, from) => {
+  if(process.server) return;
+  const authStore = useAuthStore();
+  const favStore = useFavoritesStore();
+  if(!authStore.isAuth && !favStore.storage.initialized) {
+    favStore.storage.parseStorage();
+    favStore.storage.initialized = true;
+  } else if(favStore.storage.activeItems.length > 0) {
+    await favStore.sync();
+  }
+});
