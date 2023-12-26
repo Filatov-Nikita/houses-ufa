1a805713ed07645431c8d27e99ac3073404fb735
Nikita Antonov <nikita45454@gmail.com>
2023-12-26 22:30:42 +0300

    feat: добавляет лайаут для лк агента

---
+++ layouts/lk.b2t.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="wrapper tw-py-4">
+    <PublicHeader />
+  </div>
+  <div class="section">
+    <div class="wrapper">
+      <div class="layout">
+        <AsideLeft class="layout__aside" />
+        <main class="layout__content">
+         <slot />
+       </main>
+      </div>
+    </div>
+  </div>
+  <div>
+    <div class="wrapper">
+      <PublicFooter />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import PublicHeader from '@/other-modules/public-header/index.vue';
+  import PublicFooter from '@/other-modules/public-footer/index.vue';
+  import AsideLeft from '@/lk-modules/b2t/aside/index.vue';
+</script>
+
+<style scoped lang="scss">
+  .layout {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: flex-start;
+    margin: -10px;
+
+    &__aside {
+      margin: 10px;
+      width: calc(100% / 12 * 2 - 20px);
+
+      @include lg {
+        width: calc(100% / 12 * 3 - 20px);
+      }
+
+      @include md {
+        width: calc(100% / 12 * 4 - 20px);
+      }
+
+      @include sm {
+        width: calc(100% - 20px);
+      }
+    }
+
+    &__content {
+      margin: 10px;
+      width: calc(100% / 12 * 10 - 20px);
+
+      @include lg {
+        width: calc(100% / 12 * 9 - 20px);
+      }
+
+      @include md {
+        width: calc(100% / 12 * 8 - 20px);
+      }
+
+      @include sm {
+        order: -1;
+        width: calc(100% - 20px);
+      }
+    }
+  }
+</style>
