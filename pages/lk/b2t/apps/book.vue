33e23b773f335824120015b8b1f2168e8e2cd597
Nikita Antonov <nikita45454@gmail.com>
2023-12-27 21:45:01 +0300

    лк агента - добавляет страницы на бронь и ипотеку

---
+++ pages/lk/b2t/apps/book.vue
@@ -0,0 +1,12 @@
+<template>
+  <EstateBook />
+</template>
+
+<script setup lang="ts">
+  import EstateBook from '@/lk-modules/b2t/book/index.vue';
+
+  definePageMeta({
+    layout: 'lk-b2t',
+    middleware: ['auth'],
+  });
+</script>
---
+++ pages/lk/b2t/apps/ipoteka.vue
@@ -0,0 +1,12 @@
+<template>
+  <IpotekaBlock />
+</template>
+
+<script setup lang="ts">
+  import IpotekaBlock from '@/lk-modules/b2t/ipoteka/index.vue';
+
+  definePageMeta({
+    layout: 'lk-b2t',
+    middleware: ['auth'],
+  });
+</script>
