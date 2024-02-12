b748d5c965c912db122cecfdea09201c3a91cfb5
Nikita Antonov <nikita45454@gmail.com>
2024-02-12 20:13:25 +0300

    fix: главный фильтр - убирает дубликат типа квартиры

--- other-modules/main-filter/store/flats/composables/use-flats.ts
+++ other-modules/main-filter/store/flats/composables/use-flats.ts
@@ -1,5 +1,6 @@
 import type { QueryParams } from './use-params';
 import { useSeqDataFetch } from '@/composables/useSeqDataFetch';
+import type { Flat } from '@/types/estate/flat';
 
 export function useFlats(queryParams: ComputedRef<QueryParams>) {
   const { response, next, pagination, loadingNext } = useSeqDataFetch<Flat>('estate/filter/flats', {
@@ -15,33 +16,3 @@ export function useFlats(queryParams: ComputedRef<QueryParams>) {
     next
   }
 };
-
-export interface Flat {
-  id:                       number;
-  area_total:               string;
-  floor_number:             number;
-  is_in_promotion:          boolean;
-  layout:                   string;
-  mortgage_initial_fee:     string;
-  mortgage_monthly_payment: string;
-  number_of_rooms:          number;
-  plan_image_url:           string;
-  price_total:              string;
-  status:                   string;
-  complex:                  Complex;
-  house:                    House;
-}
-
-export interface Complex {
-  is_in_city: boolean;
-  location:   string;
-  name:       string;
-  finishing_type: {
-    title: string,
-    value: string
-  },
-}
-
-export interface House {
-  letter: string;
-}
