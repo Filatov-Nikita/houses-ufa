d65aabfd6b2fc19c0adb630ec94950a08849f586
Nikita Antonov <nikita45454@gmail.com>
2024-02-21 21:57:15 +0300

    feat: добавляет форму вызвать оценщика

--- other-modules/purchase/components/trade-in/StepItems.vue
+++ other-modules/purchase/components/trade-in/StepItems.vue
@@ -1,28 +1,57 @@
 <template>
   <div class="purchase-step-items">
-    <article
-      class="purchase-card-item purchase-step-items__item"
-      v-for="(item, index) in items"
-    >
+    <article class="purchase-card-item purchase-step-items__item">
       <div class="purchase-card-item__top">
         <div class="purchase-card-item__icon-wrap">
-          <BaseIcon fit :name="item.icon" />
+          <BaseIcon fit :name="items[0].icon" />
         </div>
-        <span class="purchase-card-item__number">0{{ index + 1 }}</span>
+        <span class="purchase-card-item__number">0{{ 1 }}</span>
       </div>
       <div class="purchase-card-item__body">
-        <p>{{ item.body }}</p>
+        <p>{{ items[0].body }}</p>
       </div>
-      <div class="purchase-card-item__bottom" v-if="item.link">
-        <NuxtLink class="purchase-card-item__link" :to="item.link.to">
-          {{ item.link.label }}
+      <div class="purchase-card-item__bottom" v-if="items[0].link">
+        <NuxtLink class="purchase-card-item__link" :to="items[0].link.to">
+          {{ items[0].link.label }}
         </NuxtLink>
       </div>
     </article>
+    <article class="purchase-card-item purchase-step-items__item">
+      <div class="purchase-card-item__top">
+        <div class="purchase-card-item__icon-wrap">
+          <BaseIcon fit :name="items[1].icon" />
+        </div>
+        <span class="purchase-card-item__number">0{{ 2 }}</span>
+      </div>
+      <div class="purchase-card-item__body">
+        <p>{{ items[1].body }}</p>
+      </div>
+      <div class="purchase-card-item__bottom">
+        <button class="purchase-card-item__link" @click="zamerShowed = true">
+          Вызвать оценщика
+        </button>
+      </div>
+    </article>
+    <article class="purchase-card-item purchase-step-items__item">
+      <div class="purchase-card-item__top">
+        <div class="purchase-card-item__icon-wrap">
+          <BaseIcon fit :name="items[2].icon" />
+        </div>
+        <span class="purchase-card-item__number">0{{ 3 }}</span>
+      </div>
+      <div class="purchase-card-item__body">
+        <p>{{ items[2].body }}</p>
+      </div>
+    </article>
+    <ModalZamer v-model:showed="zamerShowed" />
   </div>
 </template>
 
 <script setup lang="ts">
+  import ModalZamer from './ModalZamer/index.vue';
+
+  const zamerShowed = ref(false);
+
   const items = [
     {
       body: 'Выбрать квартиру или дом вашей мечты и оставить заявку на услугу Трейд-ин',
@@ -35,10 +64,6 @@
     {
       body: 'Оценить вашу недвижимость при помощи нашего квалифицированного оценщика',
       icon: 'profile-full',
-      link: {
-        label: 'Вызвать оценщика',
-        to: '/main-filter',
-      }
     },
     {
       body: 'Заключить договор, если вас устроили результаты оценки, и забронировать понравившееся вам жильё',
