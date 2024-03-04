5023724f337820c766d2bd3823a371c224863ce2
Nikita Antonov <nikita45454@gmail.com>
2024-03-04 21:19:49 +0300

    добавляет карту сайта, обновляет файл роботс, обновляет пиксель

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -73,6 +73,9 @@ export default defineNuxtConfig({
         noscript: [
           {
             innerHTML: YandexMetric.noscript
+          },
+          {
+            innerHTML: Pixel.noscript
           }
         ],
         link: [
--- scripts/pixel.ts
+++ scripts/pixel.ts
@@ -1,10 +1,15 @@
 export const body = `
-  (function (d, w) {
-    var n = d.getElementsByTagName("script")[0],
-        s = d.createElement("script");
-    s.type = "text/javascript";
-    s.async = true;
-    s.src = "https://qoopler.ru/index.php?ref="+d.referrer+"&page=" + encodeURIComponent(w.location.href);
-    n.parentNode.insertBefore(s, n);
-  })(document, window);
+  var _tmr = window._tmr || (window._tmr = []);
+  _tmr.push({id: "3488145", type: "pageView", start: (new Date()).getTime()});
+  (function (d, w, id) {
+    if (d.getElementById(id)) return;
+    var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true; ts.id = id;
+    ts.src = "https://top-fwz1.mail.ru/js/code.js";
+    var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);};
+    if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); }
+  })(document, window, "tmr-code");
+`;
+
+export const noscript = `
+  <div><img src="https://top-fwz1.mail.ru/counter?id=3488145;js=na" style="position:absolute;left:-9999px;" alt="Top.Mail.Ru" /></div>
 `;
