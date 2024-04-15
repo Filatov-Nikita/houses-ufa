1608ccc2076ca957a7e6df578dacf56119b5e3e1
Nikita Antonov <nikita45454@gmail.com>
2024-04-15 20:43:49 +0500

    feat: лк агенства - добавляет страницу мотивации

---
+++ pages/lk/b2y/motivation.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="page-card">
+    <div class="section__top">
+      <h2 class="section__title">Мотивация</h2>
+    </div>
+    <MotivationList type="b2y" />
+  </section>
+</template>
+
+<script setup lang="ts">
+  import MotivationList from '@/lk-modules/motivation-list/index.vue';
+
+  definePageMeta({
+    layout: 'lk-b2y',
+    middleware: ['auth'],
+  })
+
+  useSeoMeta({
+    title: 'ЖК ЛК агентства | Мотивация',
+  });
+</script>
