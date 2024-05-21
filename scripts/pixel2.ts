3c7c1c9b3d189425f3c2e54835b056fd2e7b59c0
Nikita Antonov <nikita45454@gmail.com>
2024-05-21 11:30:29 +0500

    feat: прод - добавляет скрипт pixel

---
+++ scripts/pixel2.ts
@@ -0,0 +1,10 @@
+export const body = `
+  (function (d, w) {
+    var n = d.getElementsByTagName("script")[0],
+        s = d.createElement("script");
+    s.type = "text/javascript";
+    s.async = true;
+    s.src = "https://qoopler.ru/index.php?ref="+d.referrer+"&page=" + encodeURIComponent(w.location.href);
+    n.parentNode.insertBefore(s, n);
+  })(document, window);
+`;
