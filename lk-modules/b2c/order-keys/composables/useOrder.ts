8606c26a83f91e9fd85816e307326424bd5ca373
Nikita Antonov <nikita45454@gmail.com>
2024-07-02 17:30:17 +0500

    feat: лк клиента | заявка на ключи - добавляет useCancel, useOrder

--- lk-modules/b2c/order-keys/composables/useOrder.ts
+++ lk-modules/b2c/order-keys/composables/useOrder.ts
@@ -1,49 +1,69 @@
 import { cleanPhone } from '@/helpers';
 import { useNotifyStore } from '@/stores/notify';
+import { useTimeSlotsStore } from '../store/timeSlots';
 
 interface Body {
   visitor_phone: string,
   visitor_name: string,
-  visit_time: string,
-  visit_date: string,
-  comment: string,
 }
 
+export interface SlotResponse {
+  id: number,
+  starts_at: string,
+  ends_at: string,
+};
+
 export default function useOrder() {
+  const notify = useNotifyStore();
+  const slotStore = useTimeSlotsStore();
 
   const form: Body = reactive({
     visitor_phone: '',
     visitor_name: '',
-    visit_time: '',
-    visit_date: '',
-    comment: '',
   });
 
-  const time = ref(null);
+  const day = ref<string | null>(null);
+  const slot = ref<string | null>(null);
 
-  async function send(flatId: number, body: Body, onSuccess: () => void) {
-    const { error } = await useDataFetch(`b2c/objects/flats/${flatId}/key-obtainments`, {
+  const timeSlots = computed(() => {
+    if(slotStore.orderDays === null || day.value === null) return [];
+    const result = slotStore.orderDays.find(orderDay => orderDay.date === day.value);
+    if(!result) return [];
+    return result.slots;
+  });
+
+  async function send(flatId: number, onSuccess: (data: SlotResponse) => void) {
+    const { error, data } = await useDataFetch<{ data: SlotResponse }>(`b2c/objects/flats/${flatId}/key-acquisition`, {
       method: 'POST',
       baseURL: useRuntimeConfig().public.rootApi,
       body: {
-        ...body,
+        visitor_name: form.visitor_name,
         visitor_phone: cleanPhone(form.visitor_phone),
+        visit_datetime: slot.value!
       },
     });
 
-    const notify = useNotifyStore();
-
     if(error.value) {
-      notify.create({ type: 'error', message: 'Произошла ошибка на сервере!' });
+      notify.create({ type: 'error', message: 'Ошибка при отправке заявки, попробуйте еще раз.' });
     } else {
-      onSuccess();
-      notify.create({ type: 'success', message: 'Заявка успешно отправлена!' });
+      onSuccess(data.value!.data);
+      reset();
     }
+  }
+
+  function reset() {
+    day.value = null;
+    slot.value = null;
+    form.visitor_name = '';
+    form.visitor_phone = '';
   }
 
   return {
     form,
-    time,
+    timeSlots,
+    day,
+    slot,
     send,
+    reset,
   }
 }
