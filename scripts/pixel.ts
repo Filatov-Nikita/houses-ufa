e30192eb3377f26d37f9dd5122349cd4f108c88b
Nikita Antonov <nikita45454@gmail.com>
2026-04-17 12:47:00 +0500

    обновляет скрипт пикселя

--- scripts/pixel.ts
+++ scripts/pixel.ts
@@ -1,15 +1,15 @@
 export const body = `
   var _tmr = window._tmr || (window._tmr = []);
-  _tmr.push({id: "3488145", type: "pageView", start: (new Date()).getTime()});
-  (function (d, w, id) {
-    if (d.getElementById(id)) return;
-    var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true; ts.id = id;
-    ts.src = "https://top-fwz1.mail.ru/js/code.js";
-    var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);};
-    if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); }
-  })(document, window, "tmr-code");
+  _tmr.push({id: "3759128", type: "pageView", start: (new Date()).getTime()});
+  (function (d, w, id) { if (d.getElementById(id)) return;
+  var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true;
+  ts.id = id; ts.src = "https://top-fwz1.mail.ru/js/code.js";
+  var f = function () {var s = d.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(ts, s);};
+  if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); }
+  else { f(); } })(document, window, "tmr-code");
 `;
 
 export const noscript = `
-  <div><img src="https://top-fwz1.mail.ru/counter?id=3488145;js=na" style="position:absolute;left:-9999px;" alt="Top.Mail.Ru" /></div>
+  <div><img src="https://top-fwz1.mail.ru/counter?id=3759128;js=na" style="position:absolute;left:-9999px;" alt="Top.Mail.Ru" /></div>
 `;
